3986b8588e86039b09fae770cb027bc46a1498ad
Gabrielle Stillman <gabby@dyn-209-2-213-178.dyn.columbia.edu>
2015-07-01 15:56:24 -0400

    My first commit

---
+++ BMR Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BMR Calculator
+//
+//  Created by Gabrielle Stillman on 7/1/15.
+//  Copyright (c) 2015 Gabrielle Stillman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
